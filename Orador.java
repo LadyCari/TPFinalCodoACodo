c67a9ac7e0ee0050dd08e6a87e7cbc3c74b09823
Michelle Chen <65741018+MichelleChen2@users.noreply.github.com>
2023-12-22 01:17:32 -0300

    Codigo que tal vez sirva

---
+++ Orador.java
@@ -0,0 +1,69 @@
+
+public class Orador {
+	
+	private int id_orador; 
+	private String nombre; 
+	private String apellido; 
+	private String tema; 
+	private String mail; 
+	private String fecha_alta; 
+	
+	public Orador(int id_orador, String nombre, String apelllido, String tema, String mail, String fecha_alta) {
+		this.id_orador = id_orador; 
+		this.apellido = apellido; 
+		this.nombre = nombre; 
+		this.tema = tema; 
+		this.mail = mail; 
+		this.fecha_alta = fecha_alta; 
+	}
+	
+	public int getId() {
+		return this.id_orador; 
+	}
+	
+	public void setId(int idNuevo) {
+		this.id_orador = idNuevo; 
+	}
+	
+	public String getNombre() {
+		return this.nombre; 
+	} 
+
+	public void setNombre(String nombreNuevo) {
+		this.nombre = nombreNuevo; 
+	}
+	
+	public String getApellido() {
+		return this.apellido; 
+	} 
+
+	public void setApellido(String apellidoNuevo) {
+		this.apellido = apellidoNuevo; 
+	}
+	
+	public String getTema() {
+		return this.tema; 
+	} 
+
+	public void setTema(String temaNuevo) {
+		this.tema = temaNuevo; 
+	}
+	
+	public String getMail() {
+		return this.mail; 
+	} 
+	
+	public void setMail(String mailNuevo) {
+		this.mail = mailNuevo; 
+	}
+	
+	
+	public String getFecha() {
+		return this.fecha_alta; 
+	} 
+
+	public void setFecha(String fechaNueva) {
+		this.fecha_alta = fechaNueva; 
+	}
+	
+}
